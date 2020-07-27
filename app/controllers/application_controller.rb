977245be252293d1ad06c53e03e3552a7b17f90b
hiroya-sarwakame <kame.2146@gmail.com>
2020-07-27 19:21:32 +0900

    お気に入り機能完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # 必ずログインさせるので新しいユーザー作成時もログインしないといけない
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
@@ -14,5 +15,6 @@ class ApplicationController < ActionController::Base
     @count_microposts = user.microposts.count
     @count_followings = user.followings.count
     @count_followers = user.followers.count
+    @count_favoritings = user.favoritings.count
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,29 +7,53 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
-
   has_many :relationships
+  # 下記「through: 〜」はよく使うので予めまとめて使いやすくしておく
+  # フォローしている人をまとめる
   has_many :followings, through: :relationships, source: :follow
 
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  # フォローしてくれてる人をまとめる
   has_many :followers, through: :reverses_of_relationship, source: :user
 
+  has_many :favorites
+  # お気に入りしている投稿をまとめる
+  has_many :favoritings, through: :favorites, source: :micropost
+
+  # 自分以外のユーザーであることを確認しfollow_idを作成する
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
     end
   end
 
+  # 後ifでrelationshipの値が存在するときのみunfollowにする
   def unfollow(other_user)
     relationship = self.relationships.find_by(follow_id: other_user.id)
     relationship.destroy if relationship
   end
 
+  # self.followings内にother_userが含まれているとtureを返す
   def following?(other_user)
     self.followings.include?(other_user)
   end
 
+  # following_idsはhas_many :followings, ... によって自動的に生成されるメソッド
+  # Micropost.where(user_id: フォローユーザ + 自分自身) となる Micropost を全て取得しています。
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unfavorite(micropost)
+    favorites = self.favorites.find_by(micropost_id: micropost.id)
+    favorites.destroy if favorites
+  end
+
+  def favoriting?(micropost)
+    self.favoritings.include?(micropost)
   end
 end
