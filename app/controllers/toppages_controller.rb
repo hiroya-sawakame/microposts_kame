3696f332d98daa90444fc617d9a0db76b882a0a2
hiroya-sarwakame <kame.2146@gmail.com>
2020-10-03 23:07:22 +0900

    不要コード削除

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,7 @@
 class ToppagesController < ApplicationController
+  before_action :require_user_logged_in, only: [:index]
   def index
-    if logged_in?
-      @micropost = current_user.microposts.build
-      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
-    end
+    @micropost = current_user.microposts.build
+    @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
   end
 end
